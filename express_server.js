52a0688163390eb2e361e37fcd74db7b0206fe07
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-17 23:41:49 +0000

    adding templates html css bootstrap

--- express_server.js
+++ express_server.js
@@ -9,23 +9,34 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-  app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-  app.get("/set", (req, res) => {
-    const a = 1;
-    res.send(`a = ${a}`);
-   });
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+// app.get("/urls.json", (req, res) => {
+//     res.json(urlDatabase);
+//   });
+ 
+  // app.get("/set", (req, res) => {
+  //   const a = 1;
+  //   res.send(`a = ${a}`);
+  //  });
    
-   app.get("/fetch", (req, res) => {
-    res.send(`a = ${a}`);
-   })
+  //  app.get("/fetch", (req, res) => {
+  //   res.send(`a = ${a}`);
+  //  })
+  app.get("/hello", (req, res) => {
+    let templateVars = { greeting: 'Hello World!' };
+    res.render("hello_world", templateVars);
+  });
+   app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+  app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL:req.params.longURL  };
+    res.render("urls_show", templateVars );
+  });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
