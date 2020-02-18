a691451fddd0ff61b5bca135ef2bb14188a2599a
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-18 17:17:50 +0000

    day one completed

--- express_server.js
+++ express_server.js
@@ -3,6 +3,15 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+function generateRandomString() {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < 6; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -36,23 +45,31 @@ app.use(bodyParser.urlencoded({extended: true}));
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
    });
-  app.get("/urls/new", (req, res) => {
+   app.get("/urls/new", (req, res) => {
     res.render("urls_new");                   // ====> giving new url ejs "adding new html css attribute"
   });
   app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL:req.params.longURL  };
+    let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]  };
     res.render("urls_show", templateVars );
   });
 
-
-  
-  app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  app.get("/u/:shortURL", (req, res) => {
+   const longURL = urlDatabase[req.params.shortURL]
+    res.redirect(longURL);
   });
 
+  // app.post("/urls", (req, res) => {
+  //   console.log(req.body);  // Log the POST request body to the console
+  //   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // });
+  app.post("/urls", (req, res) => {
+    console.log(req.body);
+    let key = generateRandomString();
+    res.send({key});
+  });
   
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
