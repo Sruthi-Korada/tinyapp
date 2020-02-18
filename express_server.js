69240b201dcd6399809991456d169d058b968ba1
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-18 00:17:12 +0000

    make new url page with search box by adding form attribute

--- express_server.js
+++ express_server.js
@@ -9,10 +9,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
 // });
-// app.get("/urls.json", (req, res) => {
+// app.get("/urls.json", (req, res) => {                            // commented code are routes for the server
 //     res.json(urlDatabase);
 //   });
  
@@ -31,11 +35,23 @@ const urlDatabase = {
    app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
+   });
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");                   // ====> giving new url ejs "adding new html css attribute"
   });
   app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL:req.params.longURL  };
     res.render("urls_show", templateVars );
   });
+
+
+  
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+
+  
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
